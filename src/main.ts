cdb1c4e98b42849795065892c273bb2ae45e309a
takanome_dev <takanomedev221@gmail.com>
2023-05-23 22:01:19 +0000

    feat: add versioning and api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { major } from 'semver';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'log', 'verbose'],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: String(major('1.0.0', { loose: false })),
+  });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
