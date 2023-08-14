568cffd4c296e75e1aca33816fffd73d54f72fab
takanome_dev <takanomedev221@gmail.com>
2023-08-14 08:25:10 +0000

    fix conflicts

--- apps/server/src/common/decorators/user.decorator.ts
+++ apps/server/src/common/decorators/user.decorator.ts
@@ -1,20 +1,11 @@
 import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
 
-export const User = createParamDecorator(
-  (_: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<Request>();
-    return request.user;
-  },
-);
+export const User = createParamDecorator((_, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest<Request>();
+  return request.user;
+});
 
-// function isLocalStrategy(user?: any): user is JwtUserDto {
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-//   return user?.sub !== undefined;
-// }
-
-export const UserId = createParamDecorator(
-  (_: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<Request>();
-    return parseInt(request.user?.user_metadata.sub as string, 10);
-  },
-);
+export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest<Request>();
+  return parseInt(request.user?.user_metadata.sub as string, 10);
+});
